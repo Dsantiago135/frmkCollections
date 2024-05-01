c23a9fc252e65339efb772e301a2256365c7ede8
Dsantiago135 <mestradavid20@hotmail.com>
2024-05-01 10:43:20 -0500

    Declaración de Iterador

--- solCollections/libCollections/pkgCollections/pkgIterator/clsIterator.cs
+++ solCollections/libCollections/pkgCollections/pkgIterator/clsIterator.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using pkgServicies.pkgCollections.pkgLineal.pkgInterfaces;
+using System;
 
 namespace pkgServicies.pkgCollections.pkgIterator
 {
-    public class clsIterator<T> where T : IComparable<T>
+    public class clsIterator<T> : iIterator<T> where T : IComparable<T>
     {
         #region Attributes
         protected int attLength = 0;
@@ -10,14 +11,76 @@ namespace pkgServicies.pkgCollections.pkgIterator
         protected T attCurrentItem;
         #endregion
         #region Operations
-        public virtual bool opGo(int prmidx)
+        #region Movement
+        public bool opGoFirst()
         {
             throw new NotImplementedException();
         }
-        public bool opIsValid(int index)
+        public bool opGoPrevious()
+        {
+            throw new NotImplementedException();
+        }
+        public bool opGoMiddle()
+        {
+            throw new NotImplementedException();
+        }
+        public bool opGoNext()
+        {
+            throw new NotImplementedException();
+        }
+        public bool opGoLast()
+        {
+            throw new NotImplementedException();
+        }
+        public bool opGo(int prmIdx)
+        {
+            throw new NotImplementedException();
+        }
+        public void opGoBack()
+        {
+            throw new NotImplementedException();
+        }
+        public void opGoForward()
         {
             throw new NotImplementedException();
         }
         #endregion
+        #region Getter
+        public int opGetLength()
+        {
+            throw new NotImplementedException();
+        }
+        public int opGetCurrentIdx()
+        {
+            throw new NotImplementedException();
+        }
+        public T opGetCurrentItem()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Setter
+        public bool opSetCurrentItem(T prmContent)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Query
+        public bool opIsValid(int prmIdx)
+        {
+            throw new NotImplementedException();
+        }
+        public bool opIsThereNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool opIsTherePrevious()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+
     }
 }
---
+++ solCollections/libCollections/pkgCollections/pkgIterator/iIterator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pkgServicies.pkgCollections.pkgIterator
+{
+    internal interface iIterator<T> where T : IComparable<T>
+    {
+        #region Movement
+        bool opGoFirst();
+        bool opGoPrevious();
+        bool opGoMiddle();
+        bool opGoNext();
+        bool opGoLast();
+        bool opGo(int prmIdx);
+        void opGoBack();
+        void opGoForward();
+        #endregion
+        #region Getter
+        int opGetLength();
+        int opGetCurrentIdx();
+        T opGetCurrentItem();
+        #endregion
+        #region Setter
+        bool opSetCurrentItem(T prmContent);
+        #endregion
+        #region Query
+        bool opIsValid(int prmIdx);
+        bool opIsThereNext();
+        bool opIsTherePrevious();
+        #endregion
+    }
+}
